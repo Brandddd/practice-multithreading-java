964190b72576dd6d9657d877bc8174ce7b068df0
Brandddd <b.palacio1@utp.edu.co>
2022-09-30 00:58:49 -0500

    adding-readme

--- ThreadInterrumped/src/com/example/ThreadInterrumpedMain.java
+++ ThreadInterrumped/src/com/example/ThreadInterrumpedMain.java
@@ -5,9 +5,7 @@
         Mario Andres Ramirez
         Jefferson Ramirez
 
-        # practice-multithreading-java
-The next code, will print in a slow way increasing numbers. This will use a new thread to increase and print the number. The application must have to wait until the enter buttom its pressed by the user for interrumpt the numbers.
- */
+        */
 package com.example;
 
 /**
