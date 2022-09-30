333979877418b7029b997bd3ec17001c24080ff4
Brandddd <b.palacio1@utp.edu.co>
2022-09-30 00:49:26 -0500

    inicio-proyecto

---
+++ ThreadInterrumped/src/com/example/ThreadInterrumpedMain.java
@@ -0,0 +1,25 @@
+/*
+    Course of multithreading-java-lang
+    Programmed by:
+        Brandon Palacio Alvarez
+        Mario Andres Ramirez
+        Jefferson Ramirez
+
+        # practice-multithreading-java
+The next code, will print in a slow way increasing numbers. This will use a new thread to increase and print the number. The application must have to wait until the enter buttom its pressed by the user for interrumpt the numbers.
+ */
+package com.example;
+
+/**
+ *
+ * @author brand
+ */
+public class ThreadInterrumpedMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+
+}
