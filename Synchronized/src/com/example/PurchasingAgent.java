c6b15470dadd8974ff8d1958c65419a3952fad74
Brandddd <b.palacio1@utp.edu.co>
2022-10-03 21:55:02 -0500

    p-12-1.6

--- Synchronized/src/com/example/PurchasingAgent.java
+++ Synchronized/src/com/example/PurchasingAgent.java
@@ -2,9 +2,22 @@ package com.example;
 
 public class PurchasingAgent {
 
+    private static int cantidad;
+
+    public PurchasingAgent() {
+        cantidad = cantidad + 1;
+        System.out.println("Cantidad de instancias creadas: " + cantidad); // !Constructor que me indica la cantidad de
+                                                                           // instacias creadas de la clase
+    }
+
     public void purchase() {
         Store store = Store.getInstance();
         store.getShirtCount();
+
+        Thread.currentThread(); // !OBTENER UNA REFERENCIA AL HILO
+        double thread_id = Thread.currentThread().getId(); // !OBTENER REFERENCIA AL HILO Y SU ID
+        String thread_name = Thread.currentThread().getName(); // !OBTENER REFERENCIA AL HILO Y SU NOMBRE
+        System.out.println("Hilo ejecutandose con Codigo: " + thread_id + " y Nombre: " + thread_name);
 
         try {
             store.authorizeCreditCard("1234", 15.00);
@@ -15,7 +28,7 @@ public class PurchasingAgent {
                 System.out.print("Transaccion no aprobada. \n");
             }
         } catch (Exception e) {
-            System.out.print("Ocurrio un error al realizar la transaccion. Por favor, intente nuevamente. \n");
+            System.out.print("Ha ocurrido un error al realizar la transaccion. Por favor, intente nuevamente. \n");
         }
     }
 }
