3885e1aa24c4b4ce91c02b090ac1846f6b181c50
Brandddd <b.palacio1@utp.edu.co>
2022-10-04 18:07:11 -0500

    non-syncronized-yet

--- Synchronized/src/com/example/PurchasingAgent.java
+++ Synchronized/src/com/example/PurchasingAgent.java
@@ -2,20 +2,36 @@ package com.example;
 
 public class PurchasingAgent {
 
+    private static int contador;
+
     public PurchasingAgent() {
+        contador++;
     }
 
-    public void purchase() {
+    public static int getCantidad() {
+        return contador;
+    }
+
+    protected void finalize() throws Throwable {
+        contador--;
+        super.finalize();
+    }
+
+    public static void purchase() {
+
+        System.out.println("Cantidad de instancias de la clase: " + PurchasingAgent.getCantidad()); // *Instancias
+        System.out.println("Hilo ejecutandose con nombre " + Thread.currentThread().getName() + " y codigo "
+                + Thread.currentThread().getId());
+
         Store store = Store.getInstance(); // *Obteniendo referencia de la clase Store.
         store.getShirtCount(); // *Verifica que la tienda tiene al menos una camisa en existencia.
         store.authorizeCreditCard("1234", 15.00); // *Compra con tarjeta de crédito.
-
         Shirt shirt = store.takeShirt(); // *Tomar una camiseta de la tienda.
 
-        if (shirt != null) {   // *Comprobación
+        if (shirt != null) { // *Comprobación
             System.out.println("\nEl pago de: " + shirt + "Se ha realizado exitosamente. ");
         } else {
-            System.out.println("La transacción ha fallado. ");
+            System.out.println("La transacción ha fallado porque ha ocurrido un error. ");
         }
     }
 }
