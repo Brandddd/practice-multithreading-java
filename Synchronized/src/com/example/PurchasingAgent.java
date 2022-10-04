191253903280a4bff65d42db5c03aeefe86efc84
Brandddd <b.palacio1@utp.edu.co>
2022-10-03 21:10:13 -0500

    print-method-changes

--- Synchronized/src/com/example/PurchasingAgent.java
+++ Synchronized/src/com/example/PurchasingAgent.java
@@ -5,11 +5,17 @@ public class PurchasingAgent {
     public void purchase() {
         Store store = Store.getInstance();
         store.getShirtCount();
-        store.authorizeCreditCard("1234", 15.00);
-        Shirt shirt = store.takeShirt();
 
-        if (shirt != null) {
-            System.out.print("Producto comprado exitosamente: \n" + shirt);
+        try {
+            store.authorizeCreditCard("1234", 15.00);
+            Shirt shirt = store.takeShirt();
+            if (shirt != null) {
+                System.out.print("Producto comprado exitosamente: \n" + shirt);
+            } else {
+                System.out.print("Transaccion no aprobada. \n");
+            }
+        } catch (Exception e) {
+            System.out.print("Ocurrio un error al realizar la transaccion. Por favor, intente nuevamente. \n");
         }
     }
 }
