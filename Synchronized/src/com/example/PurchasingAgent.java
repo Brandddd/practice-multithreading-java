4f22a0402633f068ae3582148c8661beab731b96
Brandddd <b.palacio1@utp.edu.co>
2022-10-04 00:35:48 -0500

    syncronized-method-added

--- Synchronized/src/com/example/PurchasingAgent.java
+++ Synchronized/src/com/example/PurchasingAgent.java
@@ -14,20 +14,18 @@ public class PurchasingAgent {
         Store store = Store.getInstance();
         store.getShirtCount();
 
+        Thread.currentThread(); // !OBTENER UNA REFERENCIA AL HILO
         double thread_id = Thread.currentThread().getId(); // !OBTENER REFERENCIA AL HILO Y SU ID
         String thread_name = Thread.currentThread().getName(); // !OBTENER REFERENCIA AL HILO Y SU NOMBRE
         System.out.println("Hilo ejecutandose con Codigo: " + thread_id + " y Nombre: " + thread_name);
 
-        try {
-            store.authorizeCreditCard("1234", 15.00);
-            Shirt shirt = store.takeShirt();
-            if (shirt != null) {
-                System.out.print("Producto comprado exitosamente: \n" + shirt);
-            } else {
-                System.out.print("Transaccion no aprobada. \n");
-            }
-        } catch (Exception e) {
-            System.out.print("Ha ocurrido un error al realizar la transaccion. Por favor, intente nuevamente. \n");
+        Store.authorizeCreditCard("1234", 15.00);
+        Shirt shirt = store.takeShirt();
+        
+        if (shirt != null) {
+            System.out.print("Producto comprado exitosamente: \n" + shirt);
+        } else {
+            System.out.print("Transaccion no aprobada. \n");
         }
     }
 }
