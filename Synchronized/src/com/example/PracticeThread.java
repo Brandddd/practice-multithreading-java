9fd932a33e1e1872bc7da4529a2d1ebc34b264af
Brandddd <b.palacio1@utp.edu.co>
2022-10-05 14:20:28 -0500

    syncronized-thread

--- Synchronized/src/com/example/PracticeThread.java
+++ Synchronized/src/com/example/PracticeThread.java
@@ -1,10 +1,16 @@
 package com.example;
 
 public class PracticeThread extends Thread {
-    
+
+    /*
+     * AQUI LLEGAN LOS DOS HILOS CREADOS COMO P1 Y P2 TOMANDO EL ROL DE AGENTES Y
+     * LLAMAN EL METODO PurchasingAgent() SIENDO ASÍ DOS HILOS.
+     */
+
     @Override
     public void run() {
-        PurchasingAgent.purchase();
+        PurchasingAgent agent = new PurchasingAgent();
+        agent.purchase();
     }
-    
+
 }
