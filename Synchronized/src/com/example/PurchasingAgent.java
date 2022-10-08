bfa097a20a3bbd3b04f5b16c8c4c0b4bb4a19cba
Brandddd <b.palacio1@utp.edu.co>
2022-10-08 13:43:20 -0500

    synchronized-completed

--- Synchronized/src/com/example/PurchasingAgent.java
+++ Synchronized/src/com/example/PurchasingAgent.java
@@ -18,24 +18,22 @@ public class PurchasingAgent {
     }
 
     public void purchase() {
-        
-        //TODO añadir bloque sincronizado, "synchronized" en el cual entren los dos hilos y estos se sincronicen.
 
-        Store store = Store.getInstance(); // *Obteniendo referencia de la clase Store.
-
-        store.getShirtCount(); // *Verifica que la tienda tiene al menos una camisa en existencia.
-        store.authorizeCreditCard("1234", 15.00); // *Compra con tarjeta de crédito.
-        Shirt shirt = store.takeShirt(); // *Tomar una camiseta de la tienda.
-        
         System.out.println("Cantidad de instancias de la clase: " + PurchasingAgent.getCantidad()); // *Instancias
-        System.out.println("Hilo ejecutandose con nombre " + Thread.currentThread().getName() + " y codigo "
-        + Thread.currentThread().getId());
 
-        if (shirt != null) { // *Comprobación
-            System.out.println("\nEl pago de: " + shirt + "Se ha realizado exitosamente. ");
-            System.out.println();
-        } else {
-            System.out.println("No hay stock disponible. ");
+        Thread t = Thread.currentThread();
+        System.out.println("Hilo ejecutandose con nombre " + t.getName() + " y codigo " + t.getId());
+
+        Store store = Store.getInstance(); // *Obteniendo referencia de la clase Store para poder sicronizarla
+
+        synchronized (store) {
+            if (store.getShirtCount() > 0 && store.authorizeCreditCard("1234", 15.00)) {
+                Shirt shirt = store.takeShirt(); // * Toma shirt del store */
+                System.out.println("\nEl pago de: " + shirt + "Se ha realizado exitosamente. ");
+                System.out.println();
+            } else {
+                System.out.println("No hay stock disponible.");
+            }
         }
     }
 }
