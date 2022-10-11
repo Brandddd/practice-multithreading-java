b2ebc17dff71810c3ff964d764c5057f292e27f5
Brandddd <b.palacio1@utp.edu.co>
2022-10-10 19:49:36 -0500

    adding-comments

--- ThreadInterrumped/src/com/example/Counter.java
+++ ThreadInterrumped/src/com/example/Counter.java
@@ -1,10 +1,12 @@
 package com.example;
 
+//* Clase Runnable para iniciar el Hilo. */
 public class Counter extends ThreadInterrumpedMain implements Runnable {
 
     public void run() {
         int x = 0;
         while (!Thread.currentThread().isInterrupted()) {
+
             System.out.println("El actual valor de X es: " + x++);
             try {
                 Thread.sleep(1000);
